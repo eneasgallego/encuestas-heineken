1149c248a1f6d27917483d8ed29d724300c7e897
Eneas Gallego <eneasgallego@gmail.com>
2016-12-04 14:57:35 +0100

    mongodb

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,28 @@ var gulp = require('gulp');
 var bower = require('gulp-bower');
 var express = require('gulp-express');
 var rename = require('gulp-rename');
+var gutil = require('gulp-util');
 
-var config = {
-    srcDir: './src',
-    staticDir: './static',
-    destDir: './public/',
-    distDir: 'dist/',
-    configDev: './config-dev.json',
-    configPro: './config-pro.json',
-    configDist: 'config.json'
-}
+var config = require('./dirs.json');
 
 gulp.task('bower', bower);
 
 gulp.task('build-static', function() {
-    return gulp.src([config.staticDir + '/**'])
-        .pipe(gulp.dest(config.destDir));
+    return gulp.src(['.' + config.staticDir + '/**'])
+        .pipe(gulp.dest('.' + config.destDir));
 });
 gulp.task('build-src', function() {
-    return gulp.src([config.srcDir + '/**'])
-        .pipe(gulp.dest(config.destDir + config.distDir));
+    return gulp.src(['.' + config.srcDir + '/**'])
+        .pipe(gulp.dest('.' + config.destDir + '/' + config.distDir));
 });
 
 var doConfig = function(src){
-    return gulp.src(src)
+    gutil.log('src: ' + '.' + src);
+    gutil.log('configDist: ' + config.configDist);
+    gutil.log('destDir: ' + '.' + config.destDir);
+    return gulp.src('.' + src)
         .pipe(rename(config.configDist))
-        .pipe(gulp.dest(config.destDir));
+        .pipe(gulp.dest('.' + config.destDir));
 };
 gulp.task('config-dev', function(){
     return doConfig(config.configDev);
@@ -40,6 +36,24 @@ gulp.task('express', function() {
     express.run(['server.js']);
 });
 
+var exec = require('child_process').exec;
+
+function runCommand(command) {
+    return function (cb) {
+        exec(command, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb(err);
+        });
+    }
+}
+
+//Running mongo
+//http://stackoverflow.com/a/28048696/46810
+gulp.task('start-mongo', runCommand('mongod --dbpath data'));
+gulp.task('stop-mongo', runCommand('mongo --eval "use admin; db.shutdownServer();"'));
+
+
 gulp.task('build',      ['build-static', 'build-src']);
 gulp.task('build-all',  ['bower', 'build']);
 gulp.task('dev',        ['build', 'config-dev']);
@@ -47,4 +61,4 @@ gulp.task('pro',        ['build', 'config-pro']);
 gulp.task('dev-all',    ['build-all', 'config-dev']);
 gulp.task('pro-all',    ['build-all', 'config-pro']);
 
-gulp.task('default',    ['dev', 'express']);
+gulp.task('default',    ['dev', 'start-mongo', 'express']);
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // importar
 var express = require('express');
 var path = require('path');
+var config = require('./dirs.json');
 
 // instanciar
 var app = express();
 
 // ruteo
-/*
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+app.use('/', express.static(path.join(__dirname, config.destDir)));
+app.use('/', express.static(path.join(__dirname, config.destDir + '/' + config.distDir)));
+
+app.get('/config', function(req, res){
+    res.sendFile(__dirname + config.destDir + '/' + config.configDist);
 });
-*/
-app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/', express.static(path.join(__dirname, '/public/dist')));
 
 // escuchar
 app.listen(9000);
